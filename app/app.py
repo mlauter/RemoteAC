9e96c336c55f8e665bd5901f93e566d319141a7c
mlauter <lauter.miriam@gmail.com>
2014-08-28 20:56:36 -0400

    add login page and secure other routes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session, flash
 from werkzeug.contrib.fixers import ProxyFix
+from functools import wraps
 import datetime
 import time
 import db
@@ -7,13 +8,45 @@ from collections import namedtuple
 
 app = Flask(__name__, static_url_path='')
 #initialize desired_state to a dictionary representing the off state
+app.secret_key = ' \xd8\xf3yb\x80\xfc~y\xa7/\xed\x16VG,f\xf1\xc4\xb4\x14\xdfu\x08'
+
+# login required decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
 desired_state={'state_num':1,'goal_temp':''}
 
 AcState = namedtuple('AcState', ['timestamp','room_temp','is_running', 'state_num','goal_temp'])
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'miriamlauter' or request.form['password'] != 'mysupersecretpassword':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            # flash("You are now logged in!")
+            return redirect(url_for('homepage'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    # flash("You have logged out!")
+    return redirect(url_for('homepage'))
 
 @app.route('/', methods = ['GET','POST'])
 @app.route('/index', methods = ['GET','POST'])
+@login_required
 #the homepage route, reads latest entry from database and renders index.html
 def homepage():
     current_log = db.get_last_ac_state()
@@ -42,6 +75,7 @@ def update():
     return jsonify(desired_state)
 
 @app.route('/switch_state', methods=['GET','POST'])
+@login_required
 # the route the user POSTs to with desired state info
 def switch_state():
     current_log = db.get_last_ac_state()
@@ -86,4 +120,4 @@ def statify(ui_state):
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__=="__main__":
-    app.run('0.0.0.0',threaded=True)
+    app.run(debug=True,threaded=True)
